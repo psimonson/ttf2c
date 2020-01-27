d4e0348c1bed97b54e8833090bda40ccb6c90719
Philip Simonson <psimonson1988@gmail.com>
2020-01-27 07:24:47 -0500

    Sort of got it working now.

--- main.c
+++ main.c
@@ -8,12 +8,67 @@
 #include "SDL_ttf.h"
 
 #define WRITE_FILE	0	/* write to a file */
+#define NGLYPHS		128	/* number of glyphs (all printable characters) */
+/* Process the font.
+ */
+void process_font(TTF_Font *font)
+{
+	SDL_Surface *s = NULL;
+	SDL_Color color = { 0xff, 0xff, 0xff, 0x00 };
+	unsigned short ch;
+	int temp;
 
+	TTF_SetFontStyle(font, TTF_STYLE_NORMAL);
+	printf("#define BMP_NGLYPHS\t\t%d\t/* Number of glyphs */\n", NGLYPHS);
+	printf("#define BMP_PITCH\t\t%d\t/* Pitch of a glyph */\n", 8);
+	printf("const unsigned char BMP_bits[BMP_NGLYPHS][BMP_PITCH] = {\n");
+	for(ch = 0; ch < NGLYPHS; ch++) {
+		temp = TTF_GlyphIsProvided(font, ch);
+		if(temp) {
+			s = TTF_RenderGlyph_Solid(font, ch, color);
+			if(s) {
+				unsigned char *pixels = s->pixels;
+				int x, y;
+				for(y = 0; y < s->h; y++) {
+					printf("\t{ ");
+					for(x = 0; x < s->pitch; x++) {
+						printf("0x%X%s", *pixels++,
+							(x == (s->pitch-1) ? "" : ", "));
+					}
+					printf(" }%s", (y == (s->h-1) ? "\n" : ",\n"));
+				}
+				SDL_FreeSurface(s);
+			}
+		}
+	}
+	printf("};\n");
+}
+/* Program to make a C array out of a TTF font.
+ */
 int main(int argc, char **argv)
 {
+	TTF_Font *font;
 	if(argc < 2 || argc > 3) {
 		fprintf(stderr, "Usage: %s <font.ttf> <out-name>\n", argv[0]);
 		return 1;
 	}
+	if(SDL_Init(SDL_INIT_VIDEO) < 0) {
+		fprintf(stderr, "Error: %s\n", SDL_GetError());
+		return 1;
+	}
+	atexit(SDL_Quit);
+	if(TTF_Init() < 0) {
+		fprintf(stderr, "Error: %s\n", TTF_GetError());
+		return 1;
+	}
+	atexit(TTF_Quit);
+	font = TTF_OpenFont(argv[1], 10);
+	if(font == NULL) {
+		fprintf(stderr, "Error: Could not open font.\n");
+		return 1;
+	}
+	printf("Everything initialised!\n");
+	process_font(font);
+	TTF_CloseFont(font);
 	return 0;
 }
